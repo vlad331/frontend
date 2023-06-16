0e643d609c7d982da785f11ee38e549ef33bd93d
vlad331 <gggg797997@gmail.com>
2023-06-16 19:32:55 +0300

    dev

--- js/index.js
+++ js/index.js
@@ -6,3 +6,25 @@ document.addEventListener('mousemove', e =>{
         `
     })
 })
+
+let gallery_button_left = document.getElementsByClassName('gallery__button__left')[0]
+let gallery_button_right = document.getElementsByClassName('gallery__button__right')[0]
+let gallery_cards = document.getElementsByClassName('gallery__cards')[0]
+
+let card_images = [
+    'images\\entities\\card1.png',
+    'images\\entities\\card2.png',
+    'images\\entities\\card3.jpg'
+]
+for (let i = 0;i<3;i++){
+    add_card(card_images[i])
+}
+function add_card(image){
+    let new_card = document.createElement('img')
+    new_card.src = image
+    new_card.classList.add('gallery__card__pic')
+    gallery_cards.append(new_card)
+}
+gallery_button_left.addEventListener('click', e => {
+
+})
