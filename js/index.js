65d4b38684516765f63412aad8b876e96eba6239
vlad331 <gggg797997@gmail.com>
2023-06-20 19:29:59 +0300

    qq

--- js/index.js
+++ js/index.js
@@ -10,21 +10,48 @@ document.addEventListener('mousemove', e =>{
 let gallery_button_left = document.getElementsByClassName('gallery__button__left')[0]
 let gallery_button_right = document.getElementsByClassName('gallery__button__right')[0]
 let gallery_cards = document.getElementsByClassName('gallery__cards')[0]
+let card_index = 0
+let cards = []
 
 let card_images = [
     'images\\entities\\card1.png',
     'images\\entities\\card2.png',
-    'images\\entities\\card3.jpg'
+    'images\\entities\\card3.jpg',
+    'images\\entities\\card4.png',
+    'images\\entities\\png-transparent-minecraft-mob-herobrine-skeleton-著色頁2-minecraft-skeleton-angle-furniture-rectangle.png'
 ]
 for (let i = 0;i<3;i++){
-    add_card(card_images[i])
+    add_card(card_images[i], 'right')
 }
-function add_card(image){
+function add_card(image, side){
     let new_card = document.createElement('img')
     new_card.src = image
     new_card.classList.add('gallery__card__pic')
-    gallery_cards.append(new_card)
+    if(side == 'left'){
+        cards.unshift(new_card)
+        gallery_cards.prepend(new_card)
+    }
+    else{
+        cards.push(new_card)
+        gallery_cards.append(new_card)
+    }
+    
 }
+gallery_button_right.addEventListener('click', e => {
+    gallery_cards.removeChild(cards[0])
+    cards.shift()
+    card_index++
+    if(card_index > card_images.length -1){
+        card_index = 0
+    }
+    add_card(card_images[(card_index + 2) % card_images.length], 'right')
+})
 gallery_button_left.addEventListener('click', e => {
-
+    gallery_cards.removeChild(cards[2])
+    cards.pop()
+    card_index--
+    if(card_index < 0){
+        card_index = card_images.length - 1
+    }
+    add_card(card_images[card_index], 'left')
 })
