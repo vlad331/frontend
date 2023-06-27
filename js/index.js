f735294a8c28ec3325061b7bb4e9fdf62f6efcfe
vlad331 <gggg797997@gmail.com>
2023-06-27 19:31:35 +0300

    copm

--- js/index.js
+++ js/index.js
@@ -7,6 +7,15 @@ document.addEventListener('mousemove', e =>{
     })
 })
 
+class Image{
+    constructor(url, caption, descripteon, href){
+        this.url = url
+        this.caption = caption
+        this.descripteon =descripteon
+        this.href = href
+    }
+}
+
 let gallery_button_left = document.getElementsByClassName('gallery__button__left')[0]
 let gallery_button_right = document.getElementsByClassName('gallery__button__right')[0]
 let gallery_cards = document.getElementsByClassName('gallery__cards')[0]
@@ -14,19 +23,49 @@ let card_index = 0
 let cards = []
 
 let card_images = [
-    'images\\entities\\card1.png',
-    'images\\entities\\card2.png',
-    'images\\entities\\card3.jpg',
-    'images\\entities\\card4.png',
-    'images\\entities\\png-transparent-minecraft-mob-herobrine-skeleton-著色頁2-minecraft-skeleton-angle-furniture-rectangle.png'
+    new Image(
+        'images\\entities\\card1.png',
+        'хранитель',
+        'охраняет скверну',
+        'test.html',
+    ),
+    new Image(
+        'images\\entities\\card2.png',
+        'скелет на пауке',
+        'Редкий вид спавна мобов имеет все преемущества этих двух мобов',
+    ),
+    new Image(
+        'images\\entities\\card3.jpg',
+        'Корова',
+        'Корова - относится к дружелюбным мобам которые чаще всего убивают ради еды',
+    ),
+    new Image(
+        'images\\entities\\card4.png',
+        'Курятин',
+        'Курица - относится к дружелюбным мобам которые чаще всего убивают ради еды',
+    ),
+    new Image(
+        'images\\entities\\png-transparent-minecraft-mob-herobrine-skeleton-著色頁2-minecraft-skeleton-angle-furniture-rectangle.png',
+        'Визард',
+        'Визард - мини босс который имеет 3 головы и стреляет ими в врага накладывая иссушение',
+    )
 ]
 for (let i = 0;i<3;i++){
     add_card(card_images[i], 'right')
 }
 function add_card(image, side){
-    let new_card = document.createElement('img')
-    new_card.src = image
-    new_card.classList.add('gallery__card__pic')
+    let new_card = document.createElement('div')
+    new_card.innerHTML = `
+    <div class="gellary__card__caption__bg"></div>
+    <img src="${image.url}" class="gallery__card__pic">
+    <h1 class="gallery__card__caption">${image.caption}</h1>
+    <h3 class="gallery__card__description">${image.descripteon}</h3>
+    <a href="${image.href}">
+    <div class="gallery__card__href"></div>
+    </a>
+
+    `
+    new_card.classList.add("gallery__card")
     if(side == 'left'){
         cards.unshift(new_card)
         gallery_cards.prepend(new_card)
